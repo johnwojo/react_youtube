1e9ee1064efd086b7cc5c6eb5a920ef74f00cea6
johnwojo <jwojo13@gmail.com>
2019-02-01 13:06:47 -0500

    Added file path reference for the search bar in the index.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from 'search_bar';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCQdnqWgV-qyuPPE6Nhgi1-3Jr-bPTXP7M';
 
@@ -16,6 +16,5 @@ const App = () => {
 // Take this component's generated HTML and put it in the DOM.
 
 ReactDOM.render(<App />, document.querySelector('.container'));
-
 
 // 1.) Make, then 2.) show.
