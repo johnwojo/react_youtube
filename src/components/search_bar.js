48ab53c9227f96cec20ad771ff8cbf72a76f8d60
johnwojo <jwojo13@gmail.com>
2019-03-04 17:44:35 -0500

    Refactored the event handler. Simplified using an arrow method.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,13 +2,8 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    return <input onChange={this.onInputChange} />;
+    return <input onChange={(event) => console.log(event.target.value)} />;
   }
-
-  onInputChange(event){
-    console.log(event.target.value);
-  }
-
 }
 
 export default SearchBar;
