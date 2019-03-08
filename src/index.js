eb8ad25eed8ccaa60b7c4443b9641aa5f76fda32
johnwojo <jwojo13@gmail.com>
2019-03-08 12:48:42 -0500

    refactored app component to a functional component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCQdnqWgV-qyuPPE6Nhgi1-3Jr-bPTXP7M';
+
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
   console.log(data);
 });
 
 
-
-const App = () => {
-    return (
-      <div>
-        <SearchBar />
-      </div>
-    );
+class App extends Component {
+    render(){
+      return (
+        <div>
+          <SearchBar />
+        </div>
+      );
+    }
 };
 
 
