40ad4255bf15032a0021acb2dc0cb066cca5f6cd
johnwojo <jwojo13@gmail.com>
2019-03-08 12:20:07 -0500

    Imported YouTubeSearch (YTSearch) into index.js. Added a basic test to make sure it's pulling data correctly.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCQdnqWgV-qyuPPE6Nhgi1-3Jr-bPTXP7M';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
+
 
 const App = () => {
     return (
